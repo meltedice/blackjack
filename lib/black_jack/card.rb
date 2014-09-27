02b8595b5ce2c70911bbee35f9013894541ff191
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-09-27 15:26:09 +0900

    add initialize

--- lib/black_jack/card.rb
+++ lib/black_jack/card.rb
@@ -2,5 +2,17 @@
 
 module BlackJack
   class Card
+    attr_reader :name, :value
+
+    def initialize(name)
+      case name
+      when 'A'
+        @value = 1
+      when '1'..'9'
+        @value = name.to_i
+      when 'J', 'Q', 'K'
+        @value = 10
+      end
+    end
   end
 end
