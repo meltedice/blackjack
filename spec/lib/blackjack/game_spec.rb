45ea09c8856ed91d40366a1af106c6e9dfe779d8
ice <meltedise@gmail.com>
2014-09-27 21:49:55 +0900

    fix broken new game spec that didn't run any tests of it.

--- spec/lib/blackjack/game_spec.rb
+++ spec/lib/blackjack/game_spec.rb
@@ -3,9 +3,10 @@
 describe Blackjack::Game do
 
   describe "with a dealer and a player" do
+    let(:dealer) { Blackjack::Dealer.new }
+    let(:player) { Blackjack::Player.new }
     it "should start new game" do
-      expect { BlackJack::Game.new(dealer, player).not_to raise_error }
+      expect { Blackjack::Game.new(dealer, player) }.not_to raise_error
     end
   end
-
 end
